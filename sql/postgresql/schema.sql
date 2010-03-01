49e3d578af6e6749daa4620110547aaaef0b6e50
Jérôme Renard <jr@ez.no>
2010-03-01 13:58:39 +0000

    - Added Postgresql schema
    git-svn-id: http://svn.projects.ez.no/ezxmlexport/trunk@8 32376ed6-4361-46a3-a39a-35035753d010

---
+++ sql/postgresql/schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE ezxmlexport_available_contentclass_attributes (
+  contentclass_attribute_id INT   NOT NULL,
+  contentclass_id           INT   NOT NULL,
+    UNIQUE (contentclass_attribute_id ));
+
+CREATE TABLE ezxmlexport_available_contentclasses (
+  contentclass_id INT   NOT NULL,
+    UNIQUE (contentclass_id ));
+
+CREATE TABLE ezxmlexport_customers (
+  ID           SERIAL   NOT NULL,
+  NAME         VARCHAR(200)   NOT NULL,
+  ftp_target   TEXT   NOT NULL,
+  slicing_mode CHAR(1)   CHECK ( slicing_mode IN ('1','n') )   NOT NULL,
+  CONSTRAINT pk_ezxmlexport_customers PRIMARY KEY ( id ));
+
+CREATE TABLE ezxmlexport_exports (
+  ID                      SERIAL   NOT NULL,
+  customer_id             INT   NOT NULL,
+  NAME                    VARCHAR(200)   NOT NULL,
+  description             VARCHAR(200)   NOT NULL,
+  sources                 TEXT   NOT NULL,
+  ftp_target              VARCHAR(200)   NOT NULL,
+  slicing_mode            CHAR(1)   CHECK ( slicing_mode IN ('1','n') )   NOT NULL,
+  start_date              VARCHAR(15)   DEFAULT '0'   NOT NULL,
+  end_date                VARCHAR(15)   DEFAULT '0'   NOT NULL,
+  export_schedule         VARCHAR(100)   NOT NULL,
+  export_limit            INT   NOT NULL,
+  export_from_last        SMALLINT   DEFAULT '0'   NOT NULL,
+  compression             SMALLINT   NOT NULL,
+  related_object_handling SMALLINT   NOT NULL,
+  xslt_file               VARCHAR(70)   NOT NULL,
+  export_hidden_nodes     SMALLINT   DEFAULT '0'   NOT NULL,
+  CONSTRAINT pk_ezxmlexport_exports PRIMARY KEY ( id ));
+
+CREATE TABLE ezxmlexport_process_logs (
+  ID                   SERIAL   NOT NULL,
+  export_id            INT   NOT NULL,
+  start_date           VARCHAR(10)   NOT NULL,
+  end_date             VARCHAR(10)   NOT NULL,
+  start_transfert_date VARCHAR(10)   NOT NULL,
+  end_transfert_date   VARCHAR(10)   NOT NULL,
+  status               INT   NOT NULL,
+  CONSTRAINT pk_ezxmlexport_process_logs PRIMARY KEY ( id ));
+
+CREATE TABLE ezxmlexport_export_object_log (
+  process_log_id   INT   NOT NULL,
+  contentobject_id INT   NOT NULL,
+  CONSTRAINT "FK_process_log_id" FOREIGN KEY ( process_log_id ) REFERENCES ezxmlexport_process_logs(id) ON DELETE CASCADE);
