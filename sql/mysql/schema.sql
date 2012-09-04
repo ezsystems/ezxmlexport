60ffd9456f93830e3736ba4ba3ceeeda32627de5
Damien Pobel <dp@ez.no>
2012-09-04 11:12:30 +0200

    Fixed #19465: ezxmlexport: inconsistency between db_schema.dba and schema.sql

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -1,18 +1,19 @@
 CREATE TABLE ezxport_available_cclass_attr (
   contentclass_attribute_id int(11) NOT NULL,
   contentclass_id int(11) NOT NULL,
-  UNIQUE KEY contentclass_attribute_id (contentclass_attribute_id)
+  UNIQUE KEY idx_contentclass_attribute_id (contentclass_attribute_id),
+  KEY fk_contentclassattribute_id (contentclass_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ezxport_available_cclasses (
   contentclass_id int(11) NOT NULL,
-  UNIQUE KEY contentclass_id (contentclass_id)
+  UNIQUE KEY idx_contentclass_id (contentclass_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ezxport_customers (
   id int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
-  ftp_target text NOT NULL,
+  ftp_target longtext NOT NULL,
   slicing_mode CHAR(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -20,7 +21,7 @@ CREATE TABLE ezxport_customers (
 CREATE TABLE ezxport_export_object_log (
   process_log_id int(11) NOT NULL,
   contentobject_id int(11) NOT NULL,
-  KEY FK_process_log_id (process_log_id)
+  KEY fk_process_log_id (process_log_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ezxport_exports (
@@ -28,19 +29,20 @@ CREATE TABLE ezxport_exports (
   customer_id int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   description varchar(200) NOT NULL,
-  sources text NOT NULL,
-  ftp_target varchar(200) NOT NULL,
+  sources longtext NOT NULL,
+  ftp_target varchar(200) NOT NULL DEFAULT 'false',
   slicing_mode CHAR(1) NOT NULL DEFAULT '1',
   start_date varchar(15) NOT NULL DEFAULT '0',
   end_date varchar(15) NOT NULL DEFAULT '0',
-  export_schedule varchar(100) NOT NULL,
+  export_schedule varchar(100) NOT NULL DEFAULT '',
   export_limit int(11) NOT NULL,
-  export_from_last tinyint(4) NOT NULL DEFAULT '0',
-  compression tinyint(4) NOT NULL,
-  related_object_handling tinyint(4) NOT NULL,
-  xslt_file varchar(70) NOT NULL,
-  export_hidden_nodes tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (id)
+  export_from_last int(1) NOT NULL DEFAULT '0',
+  compression int(1) NOT NULL,
+  related_object_handling int(1) NOT NULL DEFAULT '0',
+  xslt_file varchar(70) DEFAULT '',
+  export_hidden_nodes int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id),
+  KEY fk_customers (customer_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ezxport_process_logs (
@@ -55,4 +57,4 @@ CREATE TABLE ezxport_process_logs (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `ezxport_export_object_log`
-  ADD CONSTRAINT FK_process_log_id FOREIGN KEY (process_log_id) REFERENCES ezxport_process_logs (id) ON DELETE CASCADE;
+  ADD CONSTRAINT fk_process_log_id FOREIGN KEY (process_log_id) REFERENCES ezxport_process_logs (id) ON DELETE CASCADE;
