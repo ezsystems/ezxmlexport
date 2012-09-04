7cb3ede397ed6a8e911554c2a58f5e84a9c70f0d
Damien Pobel <dp@ez.no>
2012-09-04 11:12:30 +0200

    Fixed #19465: ezxmlexport: inconsistency between db_schema.dba and schema.sql (cherry picked from commit 60ffd9456f93830e3736ba4ba3ceeeda32627de5)

--- sql/postgresql/schema.sql
+++ sql/postgresql/schema.sql
@@ -22,37 +22,41 @@ CREATE SEQUENCE ezxport_process_logs_s
 CREATE TABLE ezxport_available_cclass_attr (
   contentclass_attribute_id INT   NOT NULL,
   contentclass_id           INT   NOT NULL,
-    UNIQUE (contentclass_attribute_id ));
+  CONSTRAINT idx_contentclass_attribute_id  UNIQUE (contentclass_attribute_id ));
+
+CREATE INDEX fk_contentclassattribute_id ON ezxport_available_cclass_attr USING btree ( contentclass_id );
 
 CREATE TABLE ezxport_available_cclasses (
   contentclass_id INT   NOT NULL,
-    UNIQUE (contentclass_id ));
+  CONSTRAINT idx_contentclass_id UNIQUE (contentclass_id ));
 
 CREATE TABLE ezxport_customers (
   ID           integer DEFAULT nextval('ezxport_customers_s'::text) NOT NULL,
   NAME         VARCHAR(200)   NOT NULL,
-  ftp_target   TEXT   NOT NULL,
+  ftp_target   TEXT DEFAULT ''  NOT NULL,
   slicing_mode CHAR(1) DEFAULT '1' NOT NULL,
-  CONSTRAINT pk_ezxport_customers PRIMARY KEY ( id ));
+  CONSTRAINT ezxport_customers_pkey PRIMARY KEY ( id ));
 
 CREATE TABLE ezxport_exports (
   ID                      integer DEFAULT nextval('ezxport_exports_s'::text) NOT NULL,
   customer_id             INT   NOT NULL,
   NAME                    VARCHAR(200)   NOT NULL,
   description             VARCHAR(200)   NOT NULL,
-  sources                 TEXT   NOT NULL,
-  ftp_target              VARCHAR(200)   NOT NULL,
+  sources                 TEXT DEFAULT ''  NOT NULL,
+  ftp_target              VARCHAR(200) DEFAULT 'false' NOT NULL,
   slicing_mode            CHAR(1) DEFAULT '1' NOT NULL,
   start_date              VARCHAR(15)   DEFAULT '0'   NOT NULL,
   end_date                VARCHAR(15)   DEFAULT '0'   NOT NULL,
   export_schedule         VARCHAR(100)   NOT NULL,
   export_limit            INT   NOT NULL,
-  export_from_last        SMALLINT   DEFAULT '0'   NOT NULL,
-  compression             SMALLINT   NOT NULL,
-  related_object_handling SMALLINT   NOT NULL,
-  xslt_file               VARCHAR(70)   NOT NULL,
-  export_hidden_nodes     SMALLINT   DEFAULT '0'   NOT NULL,
-  CONSTRAINT pk_ezxport_exports PRIMARY KEY ( id ));
+  export_from_last        INT   DEFAULT '0'   NOT NULL,
+  compression             INT   NOT NULL,
+  related_object_handling INT   NOT NULL,
+  xslt_file               VARCHAR(70) DEFAULT '',
+  export_hidden_nodes     INT   DEFAULT '0'   NOT NULL,
+  CONSTRAINT ezxport_exports_pkey PRIMARY KEY ( id ));
+
+CREATE INDEX fk_customers ON ezxport_exports USING btree ( customer_id );
 
 CREATE TABLE ezxport_process_logs (
   ID                   integer DEFAULT nextval('ezxport_process_logs_s'::text)   NOT NULL,
@@ -62,12 +66,15 @@ CREATE TABLE ezxport_process_logs (
   start_transfert_date VARCHAR(10)   NOT NULL,
   end_transfert_date   VARCHAR(10)   NOT NULL,
   status               INT   NOT NULL,
-  CONSTRAINT pk_ezxport_process_logs PRIMARY KEY ( id ));
+  CONSTRAINT ezxport_process_logs_pkey PRIMARY KEY ( id ));
+
 
 CREATE TABLE ezxport_export_object_log (
   process_log_id   INT   NOT NULL,
   contentobject_id INT   NOT NULL,
   CONSTRAINT "FK_process_log_id" FOREIGN KEY ( process_log_id ) REFERENCES ezxport_process_logs(id) ON DELETE CASCADE);
+
+CREATE INDEX fk_process_log_id ON ezxport_export_object_log USING btree ( process_log_id );
 
 SELECT setval('ezxport_customers_s', max(id)) , 'ezxport_customers' as tablename FROM ezxport_customers;
 SELECT setval('ezxport_exports_s', max(id)) , 'ezxport_exports' as tablename FROM ezxport_exports;
